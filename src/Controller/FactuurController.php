bde2952df5974e88b29c2d90ad93544c2ca425bc
marcelvth <37689054+marcelvth@users.noreply.github.com>
2020-06-06 17:46:58 +0200

    update

--- src/Controller/FactuurController.php
+++ src/Controller/FactuurController.php
@@ -5,10 +5,13 @@ namespace App\Controller;
 use App\Entity\Factuur;
 use App\Form\FactuurType;
 use App\Repository\FactuurRepository;
+use App\Repository\RegelRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 /**
  * @Route("/factuur")
@@ -55,6 +58,38 @@ class FactuurController extends AbstractController
     {
         return $this->render('factuur/show.html.twig', [
             'factuur' => $factuur,
+        ]);
+    }
+
+    /**
+     * @Route("/pdf/{id}", name="factuur_showPDF", methods={"GET"})
+     */
+    public function showPDF(Factuur $factuur): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('factuur/show.html.twig', [
+            'factuur' => $factuur
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("fuctuur.pdf", [
+            "Attachment" => false
         ]);
     }
 
